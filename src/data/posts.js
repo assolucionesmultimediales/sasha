19162d77a868ef963956cb27591450e8e0db9ece
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-11-28 00:11:43 -0300

    se creo index, y posts.js, se modifico postcard y se borraron cosas innecesarias con la ayuda de Dawi

---
+++ src/data/posts.js
@@ -0,0 +1,32 @@
+export const misPosteos = [
+  {
+    slug: "entrada-1",
+    title: "Construcción del personaje",
+    description: "En este apartado podrás encontrar toda informacion que necesitas para poder realizar a Sasha desde 0.",
+    image: "/construccion1.jpg",
+  },
+  {
+    slug: "entrada-2",
+    title: "Exploración de poses",
+    description: "Mirá como Sasha va tomando vida y personalidad por medio de la exploración de poses dinámicas.",
+    image: "/exploracion1.png",
+  },
+  {
+    slug: "entrada-3",
+    title: "Expresiones",
+    description: "Sasha es muy expresiva, te invito a conocer sus estados de ánimo y te invito a sugerir nuevas expresiones.",
+    image: "/expresiones1.png",
+  },
+  {
+    slug: "entrada-4",
+    title: "Carta de manos",
+    description: "Las manos son una pieza fundamental dentro de nuestra protagonista, por lo cual vamos a ver en detalle ciertas poses de las mismas dentro de su actividad cotidiana.",
+    image: "/manos1.jpg",
+  },
+  {
+    slug: "entrada-5",
+    title: "Vestuario",
+    description: "Por medio de la vestimenta buscamos reflejar la personalidad y caracter de Sasha, buscando combinaciones que refuercen sus aspectos distintivos. Te invitamos a proponer nuevos outfits",
+    image: "/vestuario1.jpg",
+  },
+];
